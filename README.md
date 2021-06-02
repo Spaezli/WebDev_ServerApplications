d6727a3ac118d8bb0d40ab20aa5ba70d531e852a
Mirco Fabio Gall <mircofabio.gall@students.bfh.ch>
2021-06-02 19:12:46 +0200

    + doPost for new guest entry (within the body not the url)
    Issues:
    + GuestBookServlet.entryForm where to place the entry.html file to be able to read it
    + Back key on broser in compination with the GuestBook (add entry) does not work properly
    
    ToDo
    doGet extended
    doPost improved

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 # WebDev_ServerApplications
+
 + Java Servlets
 + RESTfull web service
-+ 
+
+# Exercise
+
+<ol>
+    <li>
+        Create a Maven project and configure the servlet dependency.
+    </li>
+    <li>
+        Extract the model classes <a href="src/guest-book.zip">guestbook.zip</a> of a guest book.
+    </li>
+    <li>
+        Write the HTML file <code>entry.html</code> that contains a form to enter the name and comment of a guest.
+    </li>
+    <li>
+        Implement the servlet class <code>GuestBookServlet</code> with the following methods:
+        <ul>
+            <li><code>doGet</code> renders all the entries of the guest book</li>
+            <li><code>doPost</code> adds a new guest book entry using the <code>guest</code> and <code>comment</code>
+                parameters
+            </li>
+        </ul>
+    </li>
+    <li>
+        Set the character encoding of the <code>HttpServletRequest</code> and <code>HttpServletResponse</code> objects
+        to UTF-8 to make sure that special characters such as umlauts are handled correctly.
+    </li>
+    <li>
+        Implement the filter class <code>SanitizingFilter</code> whose <code>doFilter</code> method verifies that the
+        request parameters only contain valid characters (optionally use the <a
+            href="https://github.com/OWASP/java-html-sanitizer" target="_blank">OWASP Java HTML Sanitizer</a>).
+    </li>
+    <li>
+        Use the locale of the <code>HttpServletRequest</code> object to format the dates of the guest book entries in
+        the client's preferred locale specified in the <code>Accept</code> header (see <a
+            href="https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html" target="_blank">DateTimeFormatter</a>
+        class).
+    </li>
+</ol>
--- src/main/java/GuestBookServlet.java
+++ src/main/java/GuestBookServlet.java
@@ -1,17 +1,55 @@
-import javax.servlet.Servlet;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 @WebServlet("/guestbook")
 public class GuestBookServlet extends HttpServlet {
     private GuestBook guestBook = new GuestBook();
-    public GuestBookServlet(){
-        guestBook.addEntry("Dimi","I loved it here");
+    private String entryForm = Files.readString(Paths.get("C:\\Users\\seven\\IdeaProjects\\WebDev_ServerApplications\\src\\main\\webapp\\pages\\entry.html"));
+    public GuestBookServlet() throws IOException {
+        guestBook.addEntry("Dimi", "I loved it here");
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
     }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        String comment = request.getParameter("comment");
+
+        guestBook.addEntry(name,comment);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("  <meta charset='UTF-8'/>");
+            out.println("  <title>GuestBook Servlet</title>");
+            out.println("  <style>* { font-family: sans-serif }</style>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println(entryForm);
+            out.println("<ul>");
+            out.println("    <li>first entry</li>");
+            guestBook.getEntries().forEach(entry -> {
+                out.println("<li>" + entry.getGuest() + " commented: " + entry.getComment() + "</li>");
+            });
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -27,10 +65,11 @@ public class GuestBookServlet extends HttpServlet {
             out.println("  <style>* { font-family: sans-serif }</style>");
             out.println("</head>");
             out.println("<body>");
+            out.println(entryForm);
             out.println("<ul>");
             out.println("    <li>first entry</li>");
             guestBook.getEntries().forEach(entry -> {
-                out.println("<li>"+ entry.getGuest() + " commented: " + entry.getComment() +"</li>");
+                out.println("<li>" + entry.getGuest() + " commented: " + entry.getComment() + "</li>");
             });
             out.println("</ul>");
             out.println("</body>");
